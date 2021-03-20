0d3cf722546a57a3e71a920ace67ef32cfecf5b6
Robert Perez <robgaray9111@yahoo.com>
2021-03-20 00:34:42 -0700

    Project set up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CalculatorPage from './pages/CalculatorPage'
+
+
+function App() {
+  return (
+    <div>
+      <CalculatorPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/CalculatorPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CalculatorPage = () => {
+    return (
+        <div>
+            I am calc
+        </div>
+    )
+}
+
+export default CalculatorPage
